23c26c6d4d7052da555990b4d25a019041cf0323
Ranjeet Kumar <ranjeetkumar70337@gmail.com>
2025-10-02 03:05:38 +0530

    dhcgsdcghsdgh

--- backend/server.js
+++ backend/server.js
@@ -64,5 +64,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
+  console.log(` Server running at http://localhost:${PORT}`);
 });
